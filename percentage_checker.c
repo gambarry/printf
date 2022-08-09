6df119247042e9e7782922e7d15638b122ff3a3f
Gentito <tsietsiramosedi6@gmail.com>
2022-08-09 06:31:01 -0700

    Percentage checker

---
+++ percentage_checker.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * percentage - function to check out for percentage in a program
+ * @valist: integer to be inputted
+ *
+ * Return: percentage
+ */
+int percentage(__attribute__((unused))va_list valist)
+{
+_putchar('%');
+return (1);
+}
